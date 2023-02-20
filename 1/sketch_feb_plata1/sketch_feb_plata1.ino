1b01d430f27ae5b4cfcf916f21c164754bcaa370
predprof1420 <125857899+predprof1420@users.noreply.github.com>
2023-02-20 13:21:18 +0300

    Add files via upload
    first commit

---
+++ 1/sketch_feb_plata1/sketch_feb_plata1.ino
@@ -0,0 +1,40 @@
+
+#include <Wire.h>
+/*
+#include <AmperkaServo.h>
+
+#include <TroykaThermometer.h>
+
+TroykaThermometer thermometer(A0);
+
+AmperkaServo servo;
+constexpr uint8_t SERVO_PIN = 9;
+constexpr auto pinSensor = A0;
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(SERVO_PIN, 544, 2400, 0, 180);
+}
+
+void loop()
+{
+  int valueSensor = analogRead(pinSensor);
+  Serial.print("Влажность почвы ");
+  Serial.println(valueSensor);
+  delay(100);
+  thermometer.read();
+  float thermometer_temp=thermometer.getTemperatureC();
+  Serial.print("Температура воздуха ");
+  Serial.print(thermometer.getTemperatureC());
+  Serial.println(" C");
+  if (thermometer_temp < 25) { 
+    Serial.println("Форточка закрыта");
+    servo.write(0);
+  } else if (thermometer_temp >= 25) { 
+    Serial.println("Форточка открыта");
+    servo.write(180);
+  }
+
+    delay(1000);
+}
+*/
